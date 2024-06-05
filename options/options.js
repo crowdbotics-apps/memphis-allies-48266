504bdd8ac62f3542d859c102fabb3d1385bccc61
Crowdbotics <team@crowdbotics.com>
2024-06-05 06:29:15 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "memphis-allies-48266",
+  url: "https://memphis-allies-48266.botics.co",
+  api: "https://memphis-allies-48266.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
